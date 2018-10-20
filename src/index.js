09e2c0aa29c801fceef352ac25e8bff0b52ca70c
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2018-10-20 17:20:27 -0400

    uses basic routing example

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { BrowserRouter as Router, Route, Link  } from 'react-router-dom'
 
 ReactDOM.render((
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>
-), document.getElementById('root'));
+  </Router>
+), document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
